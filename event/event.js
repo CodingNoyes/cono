78ce558a57e32ae77b7491be3a2ce3a91bbe0033
limjuhee0803 <limjuhee0803@naver.com>
2023-07-06 11:58:30 +0900

    임주희 파일 수정

--- event/event.js
+++ event/event.js
@@ -29,3 +29,15 @@ $('.event').each(function () {
     });
   });
 });
+
+/* hire 클릭 시 위로 부드럽게 스크롤 */
+$('a[href="#"]').on('click', (evt) => {
+  evt.preventDefault();
+});
+
+$('.bottom-btn').on('click', () => {
+  scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
