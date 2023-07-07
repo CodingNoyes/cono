ea59071e89c5c56ea5fa2dad4aa9a613f48fdcc9
nyerim <127369495+nyerim@users.noreply.github.com>
2023-07-07 13:08:50 +0900

    문예림 수정

--- branding/branding.js
+++ branding/branding.js
@@ -1,33 +1,33 @@
-// /* 마우스 휠 이벤트 */
-// $('.event').each(function () {
-//     $(this).on('wheel', function (evt) {
-//       evt.preventDefault();
+/* 마우스 휠 이벤트 */
+$('.event').each(function () {
+  $(this).on('wheel', function (evt) {
+    evt.preventDefault();
 
-//       let delta = evt.originalEvent.deltaY;
-//       let currentTop = null;
-//       const nextBox = this.nextElementSibling;
-//       const prevBox = this.previousElementSibling;
+    let delta = evt.originalEvent.deltaY;
+    let currentTop = null;
+    const nextBox = this.nextElementSibling;
+    const prevBox = this.previousElementSibling;
 
-//       if (delta > 0) {
-//         if (nextBox) {
-//           currentTop = nextBox.offsetTop;
-//         } else {
-//           currentTop = this.offsetTop;
-//         }
-//       } else {
-//         if (prevBox) {
-//           currentTop = prevBox.offsetTop;
-//         } else {
-//           return;
-//         }
-//       }
+    if (delta > 0) {
+      if (nextBox) {
+        currentTop = nextBox.offsetTop;
+      } else {
+        currentTop = this.offsetTop;
+      }
+    } else {
+      if (prevBox) {
+        currentTop = prevBox.offsetTop;
+      } else {
+        return;
+      }
+    }
 
-//       scrollTo({
-//         top: currentTop,
-//         behavior: 'smooth',
-//       });
-//     });
-//   });
+    scrollTo({
+      top: currentTop,
+      behavior: 'smooth',
+    });
+  });
+});
 
 /* hire 클릭 시 위로 부드럽게 스크롤 */
 $('a[href="#"]').on('click', (evt) => {
